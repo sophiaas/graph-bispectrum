fdf02acc481e15616244b7f4347bffbb1e8719e6
Ram Mehta <ram.mehta@gmail.com>
2018-10-23 01:52:35 -0400

    Add better print statements.

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -1,16 +1,22 @@
 import multiprocessing
+import sys
 from graphbispectrum.graph import Graph
 
-n = 7
+try:
+    n = int(sys.argv[-1])
+except:
+    n = 7
 
 def f(i):
-    return Graph.calculate_invariant_edges("bispectrum", n=n, i=i, sparse=True)
+    return i, Graph.calculate_invariant_edges("bispectrum", n=n, i=i, sparse=True)
 
 
 if __name__ == '__main__':
     iso_classes = {}
     e = int(round(n * (n - 1.0) / 2.0))
     p = multiprocessing.Pool(multiprocessing.cpu_count())
-    for iso_classes_i in p.map(f, range(e + 1)):
+    for i, iso_classes_i in p.map(f, range(e + 1)):
+        print "\t %d isomorphism classes for graphs with %d vertices and %d edges" % (len(iso_classes_i), n, i)
         iso_classes.update(iso_classes_i)
-    print "%d Isomorphism classes for graphs with %d vertices" % (len(iso_classes), n)
+    print "---"
+    print "%d isomorphism classes for graphs with %d vertices" % (len(iso_classes), n)
