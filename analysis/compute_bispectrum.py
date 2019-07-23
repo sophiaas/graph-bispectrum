033a5508fa70b6a56fb7e211ffe3d6e7071ddfdd
Sophia <sophia.sanborn@gmail.com>
2019-07-23 11:08:16 -0700

    removes non-graph-related code

--- analysis/compute_bispectrum.py
+++
@@ -1,4 +0,0 @@
-from analysis.utils.graph_analysis import *
-
-n = 7
-df = bispectrum_on_n_nodes(n, save_checkpoints=False, save_dir='analysis/output/')
--- bispectrumcode/matlab/twodbispectrum/least_squares_inverse/fconjsym.m
+++
@@ -1,8 +0,0 @@
-
-function f = fconjsym(f)
-
-[nx ny]=size(f);
-[r,c]=find(~isfinite(f));
-for i=1:length(r)
-    f(r(i),c(i)) = conj(f(mod(nx-r(i)+1,nx)+1,mod(ny-c(i)+1,ny)+1));
-end
